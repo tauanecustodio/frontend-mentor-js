30a86c1fc94694e9eb69d7e5e4177af0b52123c6
Tauane.Custodio <tauanesantoscustodio@gmail.com>
2024-06-26 10:19:17 -0300

    update logic

--- interactive-rating-component/assets/js/script.js
+++ interactive-rating-component/assets/js/script.js
@@ -1,23 +1,35 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const ratings = document.querySelectorAll('.rating-number');
-    let valueRating;
+const stars = document.querySelectorAll('input[type="radio"]');
+let starValue;
 
-    ratings.forEach((rating) => {
-        rating.addEventListener('click', function() {
-            valueRating = parseInt(this.textContent); 
-            
-            ratings.forEach((r) => r.classList.remove('selected'));
-            this.classList.add('selected');
-        });
-    });
-    
-    document.getElementById('submit-rating').addEventListener('click', () => {
-        if (isNaN(valueRating)) {
-            document.getElementById("rating-container").classList.add("not-selected");
-        } else {
-            document.getElementById("rating-state").style.display = "none";
-            document.getElementById("thank-state").style.display = "flex";
-            document.getElementById("selected-rating").textContent = valueRating;
-        };
-    });
-});
+// atribuir valor selecionado a uma váriavel
+stars.forEach((star) => {
+    star.addEventListener('click', function(e) {
+        starValue = this.value;
+    })
+})
+
+// funções
+function starNullAnimation() {
+    document.getElementById("rating-container").classList.add("not-selected");
+    setTimeout(() => {
+        document.getElementById("rating-container").classList.remove("not-selected");
+    }, "3500");
+}
+
+function submitStars(starValue) {
+    document.getElementById("rating-state").style.display = "none";             
+    document.getElementById("thank-state").style.display = "flex";
+    document.getElementById("selected-rating").textContent = starValue;
+}
+
+// evento do button submit
+document.querySelector('button[type="submit"]').addEventListener('click', function(e) {
+    e.preventDefault();
+
+    // verificar se a variavel está vazia ou não e exercutar a função
+    if (!starValue) {
+        starNullAnimation();
+    } else {
+        submitStars(starValue);
+    }
+})
